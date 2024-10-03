e72ac16ea3cabb9e21f4371d3967b2d918681615
Daniel Kennedy <danielrkennedy03@gmail.com>
2024-10-03 09:37:52 +0100

    [FEAT] Basic occurrence based search ranking

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <unordered_set>
 #include <unordered_map>
+#include <map>
 #include <utility>
 #include "index/file/FileReader.hpp"
 
@@ -19,6 +20,7 @@ int main() {
       "C:\\Users\\danie\\Development\\ISE\\cs4437\\goggle\\data\\simple_documents\\9 Hardware.txt",
     };
 
+    // Read all the documents
     std::vector<std::vector<std::string>> documents;
 
     for (std::string path : paths) {
@@ -32,6 +34,7 @@ int main() {
       std::cout << "Words: " << content.size() << std::endl;
     }
 
+    // Create an index of the documents
     std::unordered_set<std::string> unique_words;
 
     for (std::vector<std::string> content : documents) {
@@ -58,13 +61,50 @@ int main() {
       }
     }
 
-    for (std::pair<std::string, std::vector<int>> pair : index) {
-      std::cout << pair.first << ": ";
-      for (int i : pair.second) {
-        std::cout << i << " ";
+    // Index created now
+
+//    for (std::pair<std::string, std::vector<int>> pair : index) {
+//      std::cout << pair.first << ": ";
+//      for (int i : pair.second) {
+//        std::cout << i << " ";
+//      }
+//      std::cout << std::endl;
+//    }
+
+    // Now we can search the index
+    // For example, let's search for the word "computer"
+    std::string search_word = "be";
+    std::vector<int> search_results = index[search_word];
+    std::map<int, std::vector<int>> ranked_results;
+
+    for (int i = 0; i < search_results.size(); i++) {
+      if (search_results[i] > 0) {
+        ranked_results[search_results[i]].push_back(i);
       }
-      std::cout << std::endl;
     }
+
+    int rank = 1;
+
+    for (int i = ranked_results.rbegin()->first; i >= 0; i--) {
+      if (ranked_results.find(i) != ranked_results.end()) {
+        std::cout << "Rank " << rank << std::endl;
+        for (int j : ranked_results[i]) {
+          std::cout << j << " ";
+        }
+        std::cout << std::endl;
+        rank++;
+      }
+    }
+
+    std::vector<std::string> result_words = documents[ranked_results[ranked_results.rbegin()->first][0]];
+
+    for (std::string word : result_words) {
+      std::cout << word << " ";
+    }
+    std::cout << std::endl;
+
+
+    // rank the documents by the number of instances of the search word (0 means don't rank)
     
     return 0;
 }
