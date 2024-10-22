77b9ea92e5b71232c0cce135ba570a4e333f3bd3
Daniel Kennedy <danielrkennedy03@gmail.com>
2024-10-22 23:13:08 +0100

    [FIX] change output for spec

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 #include "StringUtils.hpp"
 #include "Trie.hpp"
 
-#define K 5
+#define K 10
 
 std::string frequenciesTableLocation = std::string(SERIALIZED_DATA_DIR) + std::string("/frequencies_table.bin");
 std::string vocabTrieLocation = std::string(SERIALIZED_DATA_DIR) + std::string("/vocab_trie.bin");
@@ -19,8 +19,7 @@ std::string bookPathsLocation = std::string(SERIALIZED_DATA_DIR) + std::string("
 std::string bookLengthsLocation = std::string(SERIALIZED_DATA_DIR) + std::string("/book_lengths.txt");
 std::string tableWidthLocation = std::string(SERIALIZED_DATA_DIR) + std::string("/table_width.txt");
 
-void search()
-{
+void search() {
     std::string query;
 
     std::cout << "Please enter a search term. It can be multiple words "
@@ -47,34 +46,39 @@ void search()
         bookLengthsLocation,
         tableWidthLocation);
 
-    ArrayList<Result *> *results = search.search(searchArgs, K);
+    ArrayList<Result*>* results = search.search(searchArgs);
 
-    assert(results->length == K);
 
-    std::cout << "Rank: Score (Calculated from TF-IDF) - Path" << std::endl;
+    std::string input;
+    int i = K;
+    int j = 0;
 
-    for (int i = 0; i < results->length; i++)
-    {
-        std::cout
-            << i+1 << "; "
-            << results->get(i)->score << " - "
-            << results->get(i)->name
-            << std::endl;
+    std::cout << "Rank: Title - Score (Calculated from TF-IDF) - Path" << std::endl;
+    while (i < search.file_paths->length) {
+        std::cout << "Press Enter to continue, or Ctrl-C to quit";
+        std::getline(std::cin, query);
+
+        for (; j < i; j++) {
+            std::cout
+                << j + 1 << ": "
+                << StringUtils::parseDocNameFromPath(results->get(j)->name) << " - "
+                << results->get(j)->score << " - "
+                << results->get(j)->name
+                << std::endl;
+        }
+        i += K;
     }
 }
 
-int main(int argc, char *argv[])
-{
-    if (argc != 2)
-    {
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " <index|autocomplete|search>" << std::endl;
         return 1;
     }
 
     std::string arg = argv[1];
 
-    if (arg == "index")
-    {
+    if (arg == "index") {
         Index index(GUTENBERG_DATA_DIR);
         index.index();
 
@@ -84,21 +88,15 @@ int main(int argc, char *argv[])
             bookPathsLocation,
             bookLengthsLocation,
             tableWidthLocation);
-    }
-    else if (arg == "autocomplete")
-    {
+    } else if (arg == "autocomplete") {
         int numOfWords = 0;
-        Trie *vocabTrie = new Trie(&numOfWords);
+        Trie* vocabTrie = new Trie(&numOfWords);
         vocabTrie->loadFrom(vocabTrieLocation);
         Autocomplete autocomplete(vocabTrie);
         autocomplete.start();
-    }
-    else if (arg == "search")
-    {
+    } else if (arg == "search") {
         search();
-    }
-    else
-    {
+    } else {
         std::cerr << "Invalid option: " << arg << std::endl;
         std::cerr << "Valid options are: index, autocomplete, search" << std::endl;
         return 1;
